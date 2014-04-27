9da885e67412da3e77794aaa19f1a4555aec7222
D630 <D630@users.noreply.github.com>
2014-04-27 16:32:20 +0200

    auto

---
+++ pdf-metadaten-leeren-exif-pdftk/pdf-metadaten-leeren-exif-pdftk.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Remove PDF metadata with pdftk and exiftool
+
+shopt -s nocaseglob
+
+[[ -e ./tmp ]] && { echo "There is already a tmp dir." >&2 ; exit 1 ; }
+[[ -e ./pdf_new ]] && { echo "There is already a pdf_new dir." >&2 ; exit 1 ; }
+
+mkdir -- ./tmp ./pdf_new
+cp -- ./*.pdf ./tmp
+
+while IFS= read -d '' -r
+do
+    filename=$(basename "$REPLY")
+
+    # remove XMP-Metadata incrementell
+    exiftool -all= "$REPLY"
+
+    # then rewrite PDF
+    pdftk "$REPLY" dump_data | sed -r -e 's/^(InfoValue:).*/\1/g' | pdftk "$REPLY" update_info - output "./pdf_new/${filename}" 2>/dev/null
+done < <(find ./tmp -type f -iname "*.pdf" -print0)
+
+rm -r -- ./tmp

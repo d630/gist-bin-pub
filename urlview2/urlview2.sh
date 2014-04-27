6aca7c3099ad03a61c54355c43c8b0ceca71f2dc
D630 <D630@users.noreply.github.com>
2014-04-27 16:45:04 +0200

    auto

---
+++ urlview2/urlview2.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Use choose to simulate urlview
+
+# https://github.com/vain/bin-pub/blob/master/choose
+# http://www.uninformativ.de/?section=news&ndo=single&newsid=194
+# https://github.com/vain/bin-pub/blob/master/urlview2
+
+trap '' INT HUP
+
+while read -r
+do
+    if [[ ! $DISPLAY ]]
+    then
+        elinks "$REPLY"
+    else
+        ( exec firefox -new-tab "$REPLY" & )
+    fi
+done < <(grep -oP '(((http|https|ftp|gopher)|mailto|file):(//)?[^ <>"\t]*|'\
+'(www|ftp)[0-9]?\.[-a-z0-9.]+)[^ .,;\t\n\r<">\):]?[^, <>"\t]*'\
+'[^ .,;\t\n\r<">\):]' "$@" | choose -)

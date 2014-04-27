a25849ad9eeeaf7532a77875f777446067832ca2
D630 <D630@users.noreply.github.com>
2014-04-27 16:22:36 +0200

    auto

---
+++ mlr/mlr.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Update personal non-public repos
+
+while read -r repo
+do
+    cd -- "$repo" &&
+    {
+        if [[ -e ./.git ]]
+        then
+            git ls-files --deleted -z | xargs --null git rm >/dev/null 2>&1
+            git add . >/dev/null 2>&1
+            git commit -m "mlr $(date)"
+        elif [[ -e ./.hg ]]
+        then
+            hg add >/dev/null 2>&1
+            hg commit -m "mlr $(date)"
+        fi
+    }
+done < /home/user/.mlr/repos

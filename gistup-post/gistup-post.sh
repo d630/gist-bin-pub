22225aeb665dccdc308eca372da91f8408207750
D630 <D630@users.noreply.github.com>
2014-07-04 04:34:15 +0200

    auto

--- gistup-post/gistup-post.sh
+++ gistup-post/gistup-post.sh
@@ -2,13 +2,13 @@
 
 # Postprocessing for gists on https://github.com/D630/gist-pub
 
-find "${X_XDG_CODE_DIR}/projects/gist-pub/" -mindepth 2 -maxdepth 2 -type d -name ".git" -exec rename -f 's/\.git/gistup_ign/' {} +
+find "${HOME}/stow/bin/gist-pub/" -mindepth 2 -maxdepth 2 -type d -name ".git" -exec rename -f 's/\.git/gistup_ign/' {} +
 
-cd -- "${X_XDG_CODE_DIR}/projects/gist-pub" &&
+cd -- "${HOME}/stow/bin/gist-pub" &&
 {
     git add -A .
     git commit -m "auto"
     git push origin master
 }
 
-find "${X_XDG_CODE_DIR}/projects/gist-pub/" -mindepth 2 -maxdepth 2 -type d -name "gistup_ign" -exec rename -f 's/gistup_ign/\.git/' {} +
+find "${HOME}/stow/bin/gist-pub/" -mindepth 2 -maxdepth 2 -type d -name "gistup_ign" -exec rename -f 's/gistup_ign/\.git/' {} +

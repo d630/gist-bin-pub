b794509888ecc623e3e909a60dce76676b0c875f
D630 <D630@users.noreply.github.com>
2014-07-04 08:49:24 +0200

    update

--- mail-getting/mail-getting.sh
+++ mail-getting/mail-getting.sh
@@ -9,47 +9,47 @@
 # Time in seconds between long syncs
 longsyncdiff=900 # 15 Minutes
 
-_getmail_long() { offlineimap -c "${HOME}/.offlineimaprc" -l "${HOME}/.local/log/offlineimap.log" -u quiet ; }
+__getmail_long() { offlineimap -c "${HOME}/.offlineimaprc" -l "${X_XDG_LOG_HOME}/offlineimap.log" -u quiet ; }
 
-_getmail_short() { offlineimap -o -c "${HOME}/.offlineimaprc" -l "${HOME}/.local/log/offlineimap.log" -f INBOX -u quiet ; }
+__getmail_short() { offlineimap -o -c "${HOME}/.offlineimaprc" -l "${X_XDG_LOG_HOME}/offlineimap.log" -f INBOX -u quiet ; }
 
 # -- Start.
 
 # Check connection status
-if ! ping -c1 startpage.com > /dev/null 2>&1
+if ! ping -c1 google.com 1>/dev/null 2>&1
 then
-    if ! wget -O - startpage.com > /dev/null 2>&1
+    if ! wget -O - google.com 1>/dev/null 2>&1
     then
         exit 1
     fi
 fi
 
 # Check if offlineimap is running
-(( $(pgrep -cf "/usr/bin/offlineimap") != 0 )) && { printf '%s\n' "Offlineimap is running with pid ${pid}" >&2 ; exit 1 ; }
+(($(pgrep -cf "/usr/bin/offlineimap") != 0)) && { printf '%s\n' "Offlineimap is running with pid ${pid}" 1>&2 ; exit 1 ; }
 
 # Now we determine what to do based on the last time we did things
 curtime=$(date +%s)
-if [[ -e ${HOME}/.local/log/offlineimap_lastlongsynctime ]]
+if [[ -e ${X_XDG_LOG_HOME}/offlineimap_lastlongsynctime ]]
 then
-    lastlongsync=$(< "${HOME}/.local/log/offlineimap_lastlongsynctime") >/dev/null # The unix time of the last long sync
-    timediff=$(( curtime - lastlongsync ))
-    if (( timediff > longsyncdiff ))
+    lastlongsync=$(< "${X_XDG_LOG_HOME}/offlineimap_lastlongsynctime") >/dev/null # The unix time of the last long sync
+    timediff=$((curtime - lastlongsync))
+    if ((timediff > longsyncdiff))
     then
-        printf '%s\n' "$curtime" > "${HOME}/.local/log/offlineimap_lastlongsynctime"
-        _getmail_long
+        printf '%s\n' "$curtime" > "${X_XDG_LOG_HOME}/offlineimap_lastlongsynctime"
+        __getmail_long
     else
         #_getmail_short
-        _getmail_short && rm -- "${HOME}/.mutt/mailboxes.local.uni" && cp -- "${HOME}/.mutt/_mailboxes.local.uni" "${HOME}/.mutt/mailboxes.local.uni"
+        __getmail_short && rm -- "${HOME}/.mutt/mailboxes.local.uni" && cp -- "${HOME}/.mutt/_mailboxes.local.uni" "${HOME}/.mutt/mailboxes.local.uni"
     fi
 else
-    printf '%s\n' "$curtime" > "${HOME}/.local/log/offlineimap_lastlongsynctime"
-    _getmail_long
+    printf '%s\n' "$curtime" > "${X_XDG_LOG_HOME}/offlineimap_lastlongsynctime"
+    __getmail_long
 fi
 
 # Notify new mails.
-newmails=($(find "${HOME}/Maildir/uni/" ! -empty -type d -name new))
-(( ${#newmails[@]} != 0 )) &&
+newmails=($(find "${X_MAILDIR}/uni/" ! -empty -type d -name new))
+((${#newmails[@]} != 0)) &&
 {
     notify-send -u low -h int:value:42 "[Uni] new Mails: ${#newmails[@]}"
-    aplay /usr/local/share/sounds/mac-sound-pack-new-mail.wav > /dev/null 2>&1
+    aplay /usr/local/share/sounds/mac-sound-pack-new-mail.wav 1>/dev/null 2>&1
 }

bf7445b1beb3298d1755fdb29d31efb5566a8c89
D630 <D630@users.noreply.github.com>
2014-07-07 19:59:49 +0200

    auto

--- mail-sending/mail-sending.sh
+++ mail-sending/mail-sending.sh
@@ -5,9 +5,9 @@
 shopt -s nullglob
 shopt -s dotglob
 
-_queue()
+__queue()
 {
-    declare -a f=(${HOME}/.msmtpqueue/*)
+    declare -a f=(${X_XDG_MAIL_DIR}/msmtpqueue/*)
     printf '%s\n' "${#f[@]}"
 }
 
@@ -15,15 +15,15 @@ if ! ping -c1 google.com 1>/dev/null 2>&1
 then
     if ! wget -O - google.com 1>/dev/null 2>&1
     then
-        exit 1
+        { echo "We are offline" >&2 ; exit 1 ; }
     fi
 fi
 
-(($(_queue) != 0)) &&
+(($(__queue) != 0)) &&
 {
-    until (($(_queue) == 0))
+    until (($(__queue) == 0))
     do
-        msmtp-runqueue.sh > ${X_XDG_LOG_HOME}/msmtp-queue.log 2>&1
+        msmtp-runqueue.sh > "${X_XDG_LOG_HOME}/msmtp-queue.log" 2>&1
     done
     notify-send -u low -h int:value:42 "[Uni] All mails have been sent!"
     aplay /usr/local/share/sounds/mac-sound-pack-mail-sent.wav 1>/dev/null 2>&1

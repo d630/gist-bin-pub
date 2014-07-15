a3c618097c59e1b093e8f0d476008d2c7cdfdd76
D630 <D630@users.noreply.github.com>
2014-07-15 04:33:38 +0200

    auto

---
+++ bup-daily/bup-daily.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Daily home backup with bup
+
+# -- DEBUGGING.
+
+#printf "%s (%s)\n" "$BASH_VERSION" "${BASH_VERSINFO[5]}" && exit 0
+#set -o xtrace #; exec 2>> "${X_XDG_LOG_HOME}/${1}.log
+#set -o verbose
+#set -o noexec
+#set -o errexit
+#set -o nounset
+#set -o pipefail
+#trap "(read -p "[$BASH_SOURCE:$LINENO] $BASH_COMMAND?")" DEBUG
+
+# -- SETTINGS.
+
+#declare vars_base=$(set -o posix ; set)
+
+# -- FUNCTIONS.
+
+# -- MAIN.
+
+declare -x BUP_DIR=/media/truecrypt1/ubuntu/bup
+
+while IFS= read -r -d ''
+do
+    BUP_DIR=/media/truecrypt1/ubuntu/bup/${REPLY}
+done < <(find "$BUP_DIR" -maxdepth 1 -type d -printf '%f\0' | sort -zn | sed -zn '$p')
+
+bup index -ux /home --exclude=${HOME}/stow/local/cpan/.cpan
+bup save -n home /home

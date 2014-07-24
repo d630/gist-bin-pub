b1ff62448cb8245e28cc3262df978b34d842f4eb
D630 <D630@users.noreply.github.com>
2014-07-24 23:04:24 +0200

    auto

--- obnam/obnam.sh
+++ obnam/obnam.sh
@@ -6,7 +6,36 @@ declare \
         obnam_conf=${HOME}/.obnam.conf \
         obnam_log=${X_XDG_LOG_HOME}/obnam.log
 
-sudo obnam backup --config=$obnam_conf /etc/ /opt/ /root/ /srv/ /usr/local/ /var/backups/ /var/local/ /var/log/ /var/mail/ /var/opt/ /var/www/ /var/lib/mysql/ /var/spool/ /home/
+sudo obnam backup --config=$obnam_conf \
+/etc/ \
+/home/ \
+/opt/ \
+/root/ \
+/srv/ \
+/usr/lib/ \
+/usr/local/ \
+/usr/share/applications/ \
+/usr/share/desktop-directories/ \
+/usr/share/fonts/ \
+/usr/share/images/ \
+/usr/share/keyrings/ \
+/usr/share/menu/ \
+/usr/share/mime/ \
+/usr/share/mime-info/ \
+/usr/share/mimelnk/ \
+/usr/share/plymouth/ \
+/usr/share/sounds/ \
+/usr/share/themes/ \
+/usr/share/wallpapers/ \
+/usr/share/xsessions/ \
+/usr/src/ \
+/var/backups/ \
+/var/lib/ \
+/var/local/ \
+/var/log/ \
+/var/mail/ \
+/var/opt/ \
+/var/www/ \
 
 sudo obnam force-lock --config=$obnam_conf
 

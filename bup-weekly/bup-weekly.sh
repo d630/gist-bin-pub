5cd9758b89d3924f4ebcb30128d9d09c537abf19
D630 <D630@users.noreply.github.com>
2014-07-27 14:49:16 +0200

    auto

--- bup-weekly/bup-weekly.sh
+++ bup-weekly/bup-weekly.sh
@@ -3,7 +3,7 @@
 # Weekly home backup with bup
 
 declare -x \
-           BUP_DIR=/media/truecrypt1/ubuntu/bup \
+           BUP_DIR=/media/truecrypt1/linux/bup \
            today=$(date +%Y-%m-%d)
 
 while IFS= read -r -d ''
@@ -15,7 +15,7 @@ do
     printf '%s\n' "${BUP_DIR}/${REPLY} has been deleted." 1>&2
 done < <(find "$BUP_DIR" -maxdepth 1 -type d -printf '%f\0' | sort -zn | sed -zn '$!p')
 
-BUP_DIR=/media/truecrypt1/ubuntu/bup/${today}
+BUP_DIR=/media/truecrypt1/linux/bup/${today}
 
 bup init
 bup index -ux /home --exclude=${HOME}/stow/local/cpan/.cpan

0278ef32c520eca2dc32151f6d387d915eaf51da
D630 <D630@users.noreply.github.com>
2014-08-04 20:31:49 +0200

    auto

--- print-horiz/print-horiz.sh
+++ print-horiz/print-horiz.sh
@@ -2,5 +2,6 @@
 
 # Print horizontal line (http://wiki.bash-hackers.org/snipplets/print_horizontal_line)
 
-printf '%*s\n' "${1:-$(tput cols)}" ' ' | tr ' ' -
+#printf '%*s\n' "${1:-$(tput cols)}" ' ' | tr ' ' -
+printf -v horiz '%*s\n%s' "${1:-$(tput cols)}" '' ; printf '%s' "${horiz// /-}"
 #eval printf %.0s- '{1..'"${1:-$(tput cols)}"\}; echo

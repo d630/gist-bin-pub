2fd16ffc91e028f7f2e777b9539fe4a313a7c8f2
D630 <D630@users.noreply.github.com>
2014-08-19 11:11:35 +0200

    auto

--- mutt-uri-filter/mutt-uri-filter.sh
+++ mutt-uri-filter/mutt-uri-filter.sh
@@ -6,5 +6,5 @@
 
 while read -r
 do
-    taskum add uri:"$REPLY"
+    um add uri="$REPLY"
 done < <(grep -oP -e '((http|https|gopher|ftp|ftps|webdav|webdavs|dav|davs):(//)?[^ <>"\t]*|(www|ftp)[0-9]?\.[-a-z0-9.]+)[^ .,;\t\n\r<">\):]?[^, <>"\t]*[^ .,;\t\n\r<">\):]')

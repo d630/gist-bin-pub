24f20cc3986f4baa60035cde087b9010b623b1f5
D630 <D630@users.noreply.github.com>
2014-08-19 11:09:12 +0200

    auto

--- backup-firefox-urls/backup-firefox-urls.sh
+++ backup-firefox-urls/backup-firefox-urls.sh
@@ -6,15 +6,17 @@ db_path=$(echo ${HOME}/.mozilla/firefox/*.default)/places.sqlite
 log=${X_XDG_LOG_HOME}/urls.log
 uris=${X_XDG_LOG_HOME}/uris_new.log
 
-sqlite3 "$db_path" 'SELECT DISTINCT moz_places.url FROM moz_places WHERE (url LIKE "http%" OR url LIKE "ftp%" OR url LIKE "file%" OR url LIKE "mailto%" OR url LIKE "gopher%") UNION ALL SELECT DISTINCT moz_favicons.url FROM moz_favicons WHERE (url LIKE "http%" OR url LIKE "ftp%" OR url LIKE "file%" OR url LIKE "mailto%" OR url LIKE "gopher%") ORDER BY url' \
-| sed 's/"//g' \
-| tee >(cat "$log" - | sort | uniq > "${log}.$$" && mv "${log}.$$" "$log") > "$uris"
+sqlite3 "$db_path" 'SELECT DISTINCT moz_places.url FROM moz_places WHERE (url LIKE "http%" OR url LIKE "ftp%" OR url LIKE "file%" OR url LIKE "mailto%" OR url LIKE "gopher%") UNION ALL SELECT DISTINCT moz_favicons.url FROM moz_favicons WHERE (url LIKE "http%" OR url LIKE "ftp%" OR url LIKE "file%" OR url LIKE "mailto%" OR url LIKE "gopher%") ORDER BY url' | \
+    sed 's/"//g' | \
+    tee \
+    >(cat "$log" - | sort | uniq > "${log}.$$" && mv "${log}.$$" "$log") \
+    > "$uris"
 
 [[ -e $uris ]] &&
 {
     while read -r line
     do
-        taskum add uri:"${line}"
+        um add uri="${line}"
     done < "$uris"
     rm -- "$uris"
 }

8cc1c3e9b3c3d5a28baaa88a4de5a00c989501a5
D630 <D630@users.noreply.github.com>
2014-08-29 00:39:14 +0200

    auto

--- mb2md/mb2md.sh
+++ mb2md/mb2md.sh
@@ -6,13 +6,7 @@
 # 2. In Thunderbird (Archiv) eml importieren und archivieren
 # 3. In Thunderbird (Archiv) Archiv als mbox exportieren
 
-for i in 1970 2007 2008 2009 2010 2011 2012 2013 2014
+for i in 1970 {2007..2014}
 do
-    mb2md -s ${HOME}/tmp/Archiv/${i} -R -d ${HOME}/tmp/new/${i}
+    mb2md -s "${X_XDG_TMP_HOME}/Archiv/${i}" -R -d "${X_XDG_TMP_HOME}/md/${i}"
 done
-
-# Überflüssige Dateien löschen .sdb-Extension entfernen
-
-find . -type d ! -name cur ! -name new ! -name tmp -name ".*" | xargs -r rename 's/\/\./\//'
-
-# !!! Weg finden, um das Konvertieren zu verhindern. Stattdessen: Maildir aus Mutt direkt in Mutt (Archiv) verschieben

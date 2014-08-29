28f1e821f341b90e50bc8899512791278d156624
D630 <D630@users.noreply.github.com>
2014-08-30 00:28:46 +0200

    auto

--- xtermj2/xtermj2.sh
+++ xtermj2/xtermj2.sh
@@ -8,11 +8,11 @@
 while read -r _ id
 do
     [[ $(xprop -id "${id//:/}" WM_CLASS) =~ xtermj2 ]] && xid=${id//:/} && break
-done < <(xlsclients -l | fgrep -x -e '  Command:  xterm -name xtermj2' -B 4 | grep -e '^Window')
+done < <(xlsclients -l | fgrep -x -e '  Command:  xterm -b 4 -name xtermj2' -B 4 | grep -e '^Window')
 
 if [[ ! $xid ]]
 then
-    xterm -name xtermj2
+    xterm -b 4 -name xtermj2
 else
     if ! xprop -root _NET_CLIENT_LIST | grep -q "$xid"
     then

6a5f589250fe7279aca2899b55b382707fe8534e
D630 <D630@users.noreply.github.com>
2014-09-06 07:42:34 +0200

    auto

--- mediathekview/mediathekview.sh
+++ mediathekview/mediathekview.sh
@@ -2,4 +2,4 @@
 
 # MediathekView (german tv)
 
-${HOME}/var/home/code/java/MediathekView_4/MediathekView__Linux.sh -auto -noGui
+${X_XDG_CODE_DIR}/java/MediathekView_4/MediathekView__Linux.sh -auto -noGui

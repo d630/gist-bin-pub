e2c84c0f58b60a2e518f9eec10a4295aa5edfb1b
D630 <D630@users.noreply.github.com>
2014-10-07 00:32:57 +0200

    auto

--- switch-to-desktop/switch-to-desktop.sh
+++ switch-to-desktop/switch-to-desktop.sh
@@ -2,5 +2,5 @@
 
 # select desktop with a menu and then switch with wmctrl to it
 
-declare desk_number=$(wmctrl -d | menu.sh "dmenu2" ">")
-wmctrl -s "${desk_number%% *}"
+declare desk=$(wmctrl -d | menu.sh "dmenu2" ">")
+wmctrl -s "${desk%% *}"

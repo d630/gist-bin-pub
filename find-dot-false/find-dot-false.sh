ab8665cb958f43d8f0c88e56f3052afe774303d1
D630 <D630@users.noreply.github.com>
2014-10-08 10:11:09 +0200

    auto

---
+++ find-dot-false/find-dot-false.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# find files which are currently not used with stow
+
+find "$HOME" "${HOME}/etc" "${HOME}/share" \
+        -mindepth 1 \
+        -maxdepth 1  \
+        ! -type l  \
+        ! -name share \
+        ! -name etc \
+        ! -name bin \
+        ! -name stow \
+        ! -name var \
+        ! -name .git \
+        ! -print

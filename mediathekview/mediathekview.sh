7bea7072dfb5699f91eb6b135b60bbdef3ec39f6
D630 <D630@users.noreply.github.com>
2014-10-09 18:19:52 +0200

    auto

--- mediathekview/mediathekview.sh
+++ mediathekview/mediathekview.sh
@@ -2,4 +2,13 @@
 
 # MediathekView (german tv)
 
-${X_XDG_CODE_DIR}/java/MediathekView_4/MediathekView__Linux.sh -auto -noGui
+case $1 in
+    run)
+        java -jar "${X_XDG_CODE_DIR}/java/MediathekView_4/MediathekView.jar"
+        ;;
+    load)
+        "${X_XDG_CODE_DIR}/java/MediathekView_4/MediathekView__Linux.sh" -auto -noGui
+        ;;
+    *)
+        { echo "Usage: mediathekiew.sh (run|load)" 1>&2 ; exit 1 ; }
+esac

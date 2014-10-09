efcdcbde11ab42a3307a31564cd99520fda37523
D630 <D630@users.noreply.github.com>
2014-10-09 19:33:11 +0200

    auto

--- export-x-info/export-x-info.sh
+++ export-x-info/export-x-info.sh
@@ -3,7 +3,7 @@
 # Export the dbus session address on startup so it can be used by cron
 # Based on: http://dev.gentoo.org/~tomka/mail.html
 
-> "${XDG_DATA_HOME}/xdbus"
-chmod 600 "${XDG_DATA_HOME}/xdbus"
-env | grep DBUS_SESSION_BUS_ADDRESS >> "${XDG_DATA_HOME}/xdbus"
-printf '%s\n' "export DBUS_SESSION_BUS_ADDRESS" >> "${XDG_DATA_HOME}/xdbus"
+> "${X_XDG_LIB_DIR}/xdbus"
+chmod 600 "${X_XDG_LIB_DIR}/xdbus"
+env | grep DBUS_SESSION_BUS_ADDRESS >> "${X_XDG_LIB_DIR}/xdbus"
+printf '%s\n' "export DBUS_SESSION_BUS_ADDRESS" >> "${X_XDG_LIB_DIR}/xdbus"

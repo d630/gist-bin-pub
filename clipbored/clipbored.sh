69ca7ef944312f66124f2ee3f47eef5f3e416e17
D630 <D630@users.noreply.github.com>
2014-10-09 17:10:55 +0200

    auto

--- clipbored/clipbored.sh
+++ clipbored/clipbored.sh
@@ -2,7 +2,7 @@
 
 # clipboard manager (dmenu) for clipbored (https://github.com/trapd00r/clipbored)
 
-declare xsels=${XDG_DATA_HOME}/clipbored/clips
+declare xsels=${X_XDG_LIB_DIR}/clipbored/clips
 
 printf -v xsels '%s' "$(tac "$xsels" | sed '/^$/d' | menu.sh "dmenu2" ">")"
 printf "$xsels" | xsel -ib

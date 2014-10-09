10ef7466715a4c9ab8ecffdbf509c66fa71f7e9d
D630 <D630@users.noreply.github.com>
2014-10-09 19:35:43 +0200

    auto

--- task2remind/task2remind.sh
+++ task2remind/task2remind.sh
@@ -5,4 +5,7 @@
 # modified line in task_export-ical.pl :
 # my $command = join (' ', ("env PATH=$ENV{PATH} task rc:/home/user/.taskrc rc.verbose=nothing rc.json.array=no due.after:2013-01-01_00:01 and '(status:pending or status:waiting)' export", @ARGV));
 
-ical2rem-cli -t --lead-time "" < <(task-export-ical.pl | grep -v "PRIORITY:") | sed 's#[[:blank:]][[:blank:]]##g' > "${XDG_DATA_HOME}/ical2rem"
+ical2rem-cli -t --lead-time "" \
+    < <(task-export-ical.pl | grep -v "PRIORITY:") | \
+    sed 's#[[:blank:]][[:blank:]]##g' \
+    > "${X_XDG_LIB_DIR}/ical2rem"

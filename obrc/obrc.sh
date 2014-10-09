a45eb513fc45aa1705f58eb8fe62e59a9c234725
D630 <D630@users.noreply.github.com>
2014-10-09 21:47:23 +0200

    auto

--- obrc/obrc.sh
+++ obrc/obrc.sh
@@ -5,41 +5,29 @@
 case $1 in
     -c|--cat)
         command cat \
-            <(builtin printf '%s\n' '<?xml version="1.0" encoding="UTF-8"?>' '<openbox_config xmlns="http://openbox.org/3.4/rc">') \
+            <(builtin printf '%s\n' '<?xml version="1.0" encoding="UTF-8"?>' '<openbox_config xmlns="http://openbox.org/3.5/rc">') \
             "${XDG_CONFIG_HOME}"/openbox/rc-*.xml \
             <(builtin printf '%s\n' '</openbox_config>') \
             > "${XDG_CONFIG_HOME}/openbox/rc.xml"
         ;;
     -s|--split)
-        builtin declare \
-            i= \
-            REPLY=
+        builtin declare i=
         builtin declare -a "sections=(
             applications
-            menu
-            resistance
-            placement
-            margins
-            focus
             desktops
-            mouse
-            keyboard
             dock
+            focus
+            keyboard
+            margins
+            menu
+            mouse
+            placement
+            resistance
             resize
             theme)"
-
-        #~ while builtin read -r
-        #~ do
-            #~ i=${REPLY#rc-}
-            #~ command sed -n "/<${i%*.xml}>/,/<\/${i%*.xml}>/ p" "${XDG_CONFIG_HOME}/openbox/rc.xml" \
-                #~ > "${XDG_CONFIG_HOME}/openbox/${REPLY}"
-        #~ done < <(command find "${XDG_CONFIG_HOME}/openbox/" \
-            #~ -name "rc-*.xml" \
-            #~ -type f \
-            #~ -printf '%P\n')
         for i in "${sections[@]}"
         do
-            command sed -n "/<${i}>/,/<\/${i}>/ p" "${XDG_CONFIG_HOME}/openbox/rc.xml" \
+            command sed -n "/<${i}> <!-- ${i^^} -->/,/<\/${i}>/ p" "${XDG_CONFIG_HOME}/openbox/rc.xml" \
                 > "${XDG_CONFIG_HOME}/openbox/rc-${i}.xml"
         done
         ;;

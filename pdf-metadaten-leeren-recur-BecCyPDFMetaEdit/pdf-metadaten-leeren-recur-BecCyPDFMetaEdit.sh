49c736ecb6a50a6cfcc03443aeda9658aeb54cd0
D630 <D630@users.noreply.github.com>
2014-10-20 01:00:18 +0200

    auto

--- pdf-metadaten-leeren-recur-BecCyPDFMetaEdit/pdf-metadaten-leeren-recur-BecCyPDFMetaEdit.sh
+++ pdf-metadaten-leeren-recur-BecCyPDFMetaEdit/pdf-metadaten-leeren-recur-BecCyPDFMetaEdit.sh
@@ -2,4 +2,4 @@
 
 # Remove PDF metadat with BeCyPDFMetaEdit (recur)
 
-wine "${HOME}/.wine/drive_c/Program Files (x86)/BeCyPDFMetaEdit/BeCyPDFMetaEdit.exe" "./*.pdf" -s -d 2 -T "" -S "" -A "" -K "" -R "" -P "" -C "" -M "" -X 1 -q
+wine "${HOME}/.wine/drive_c/Program Files/BeCyPDFMetaEdit/BeCyPDFMetaEdit.exe" "./*.pdf" -s -d 2 -T "" -S "" -A "" -K "" -R "" -P "" -C "" -M "" -X 1 -q

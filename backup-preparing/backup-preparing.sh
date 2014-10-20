fa4021469a53ee133d509f6a0b256f4c8d4b9c74
D630 <D630@users.noreply.github.com>
2014-10-20 02:48:30 +0200

    auto

--- backup-preparing/backup-preparing.sh
+++ backup-preparing/backup-preparing.sh
@@ -2,8 +2,8 @@
 
 # Prepare full backup for Meinecke
 
-# Closing
-{ printf -v horiz '%*s\n%s' "80" '' "Closing" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
+# Ini
+{ printf -v horiz '%*s\n%s' "80" '' "Ini" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 clipbored -k
 pkill clipbored
 pkill -f clipbored-schleife.sh
@@ -12,7 +12,6 @@ pkill -f backup-firefox-history-schleife.sh
 
 # Connection
 #{ printf -v horiz '%*s\n%s' "80" '' "Connection" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
-#ssh-add "${HOME}/.ssh/id_rsa_backup"
 
 # MySql
 #{ printf -v horiz '%*s\n%s' "80" '' "MySql" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
@@ -21,15 +20,14 @@ pkill -f backup-firefox-history-schleife.sh
 { printf -v horiz '%*s\n%s' "80" '' "mlr" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 mlr.sh
 
-# Paketlisten && Repos
-{ printf -v horiz '%*s\n%s' "80" '' "Packages&Repos" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
+# Packages & Repos
+{ printf -v horiz '%*s\n%s' "80" '' "Packages & Repos" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 sudo dpkg --clear-avail
 sudo apt-get autoclean
 sudo apt-get autoremove
 sudo dpkg --configure -a
 sudo apt-get install -f
-#mr -j5 update
-#cd -- "${X_XDG_CODE_DIR}/vcs/org-mode" && make uncompiled
+mr -j5 update
 sudo rm -r -- "${X_XDG_BACKUPS_DIR}/paketlisten" &&
 mkdir -- "${X_XDG_BACKUPS_DIR}/paketlisten" &&
 cd -- "${X_XDG_BACKUPS_DIR}/paketlisten" &&
@@ -42,36 +40,35 @@ cd -- "${X_XDG_BACKUPS_DIR}/paketlisten" &&
     sudo cp -- "/etc/apt/trusted.gpg" "trusted-keys.gpg"
 }
 
-# Programme
-{ printf -v horiz '%*s\n%s' "80" '' "Programme" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
+# Programs
+{ printf -v horiz '%*s\n%s' "80" '' "Programs" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 ls $(printf '%s ' ${PATH//:/ }) "/usr/bin/X11" | xargs whatis | sort | uniq > "${X_XDG_BACKUPS_DIR}/Programme.txt"
 
-# Anzeige einer ausführlichen Übersicht der aktivierten Quellen mit den Apt-Pinning-Informationen
+# Apt
 { printf -v horiz '%*s\n%s' "80" '' "Apt" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 apt-cache policy > "${X_XDG_BACKUPS_DIR}/apt-quellen.txt"
 
-# Liste der in den letzten 7 Tagen installierten Pakete
+# Last 7 Days
 { printf -v horiz '%*s\n%s' "80" '' "Last 7 Days" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 find "/var/lib/dpkg/info/" -daystart \( -name \*.list -a -mtime -7 \) | sed 's/.list$//;s/.*\///' | sort > "${X_XDG_BACKUPS_DIR}/software_7_days.txt"
 
-# init-Liste
-{ printf -v horiz '%*s\n%s' "80" '' "Inits" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
+# Init
+{ printf -v horiz '%*s\n%s' "80" '' "Init" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 { find "/etc/init.d/" -perm /111 -type f -printf '%f\n'; while IFS= read -r -d '' ; do printf '%s\n' "${REPLY%*.*}" ; done < <(find "/etc/init" -type f -printf '%f\0') ; } | sort -u > "${X_XDG_BACKUPS_DIR}/dienste.txt"
 
 # etckeeper
 { printf -v horiz '%*s\n%s' "80" '' "etckeeper" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 cd -- "/etc" &&
 {
-    sudo git ls-files --deleted -z | xargs -0 git rm 1>/dev/null 2>&1
     sudo git add -A . 1>/dev/null 2>&1
     sudo git commit -a -m "$(date): backup preparing"
 }
 
-# exif
+# Publikationen-Metadaten
 { printf -v horiz '%*s\n%s' "80" '' "Publikationen-Metadaten" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 exiftool-publikationen-metadaten-erstellen.sh
 
-# taskwarrior
+# Taskwarrior
 { printf -v horiz '%*s\n%s' "80" '' "Taskwarrior" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 task-makedirs.sh
 
@@ -80,15 +77,15 @@ task-makedirs.sh
 #rsync-repositorium-backup-alt.sh
 #rsync-repositorium-backup.sh
 
-# pdnsd abfragen
+# pdnsd
 #{ printf -v horiz '%*s\n%s' "80" '' "pdnsd" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 
-# passivedns.log sichern
+# passivedns
 { printf -v horiz '%*s\n%s' "80" '' "passivedns" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 passivedns-archiv.sh
 sudo cp -- /var/log/passivedns-archive/*.gz "${X_XDG_LOG_HOME}/passivedns"
 
-# backup firefox urls
+# firefox-urls
 { printf -v horiz '%*s\n%s' "80" '' "firefox-urls" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 backup-firefox-urls.sh
 
@@ -104,19 +101,11 @@ do
 done
 shopt -u extglob
 
-# Delete Firexox places.sqlite
-#{ printf -v horiz '%*s\n%s' "80" '' "Delete Firefox places.sqlite" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
-#find -H "${HOME}/.mozilla/firefox" -type f -name "places.sqlite" -delete
-
-# profile cleaner
+# ProfileCleaner
 #{ printf -v horiz '%*s\n%s' "80" '' "ProfileCleaner" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
-#profile-cleaner f
-#profile-cleaner p "${HOME}/.newsbeuter"
-#profile-cleaner p "${HOME}/.thunderbird"
-#profile-cleaner p "${HOME}/.config/qupzilla/profiles"
 
-# Delete /var/log/pm-powersave.log
-{ printf -v horiz '%*s\n%s' "80" '' "DELETE:'/var/log/pm-powersave.log'" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
+# Delete pm-powersave.log
+{ printf -v horiz '%*s\n%s' "80" '' "Delete powersave.log'" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 sudo rm -r -- "/var/log/pm-powersave.log"
 
 # ccleaner
@@ -126,7 +115,6 @@ cclean.sh
 # Mail-Report
 { printf -v horiz '%*s\n%s' "80" '' "Mail-Report" ; printf '%s\n' "${horiz// /-}" ; } 1>&2
 logfile=${X_XDG_LOG_HOME}/backup-preparing.log
-empfaenger=backup-preparing@backup-preparing
-betreff="BackupPreparing-log"
-echo "$(date +"%Y"-"%m"-"%d"_"%R")" >> "$logfile"
-tail -n1 "$logfile" | mail -s "$betreff" "$empfaenger"
+to=backup-preparing@backup-preparing
+subj="BackupPreparing-log"
+echo "$(date +"%Y"-"%m"-"%d"_"%R")" | tee >(mail -s "$subj" "$to") >> "$logfile"

a6be3aa8f27a86433a3d0a25aeecf0fbe46e1ff3
D630 <D630@users.noreply.github.com>
2014-10-30 19:21:24 +0100

    auto

--- wecker-gui/wecker-gui.sh
+++ wecker-gui/wecker-gui.sh
@@ -6,7 +6,7 @@ declare -A music
 music[radio1]='http://www.radioeins.de/live.m3u'
 music[open]="-R ${XDG_MUSIC_DIR}/open_your_eyes.mp3"
 
-datum=$(date +%F)
+datum=$(printf '%s\n' "now" "tomorrow" | menu.sh "dmenu2" "Datum" | date -d - +%F)
 tempus=$(printf '%s\n' {00..23}:{00..59} | menu.sh "dmenu2" "Zeitpunkt")
 zp="${datum} ${tempus}"
 stream=$(printf '%s\n' "${!music[@]}" | menu.sh "dmenu2" "Stream")

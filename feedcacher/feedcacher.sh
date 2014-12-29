e020cb4bcf7405d200345e731081f0cee53f1371
D630 <D630@users.noreply.github.com>
2014-12-29 07:42:48 +0100

    feedcacher: remove deps

--- feedcacher/feedcacher.sh
+++ feedcacher/feedcacher.sh
@@ -3,34 +3,39 @@
 # Simple feed downloader based on urldiff (https://github.com/D630/urldiff)
 
 declare -r feedcacherdir=${X_XDG_LIB_DIR}/feedcacher
-declare -r cachedir=${feedcacherdir}/cache
+declare -xr cachedir=${feedcacherdir}/cache
 declare -r urllist=${feedcacherdir}/urls.txt
-declare -r reportnew=${feedcacherdir}/report.txt
-declare -r table=${feedcacherdir}/table.txt
-
-export cachedir \
-        reportnew \
-        table
+declare -xr reportnew=${feedcacherdir}/report.txt
+declare -xr table=${feedcacherdir}/table.txt
 
 __feedcacher_do()
 {
-    url=$1
-    if [[ $url == http*://*.* ]]
+    declare \
+        filename= \
+        onlinexml= \
+        url=${1,,}
+
+    [[ $url == \#* ]] && return 1
+
+    if [[ $url =~ ^https?://.*\.[a-z]*/.*$ ]]
     then
-        filename=$(sed 's#_#-#g;s# #_#g;s#\/#_#g' <<< "$url" | colrm 121)
+        filename=${url:0:121}
+        filename=${filename//_/-}
+        filename=${filename// /_}
+        filename=${filename//\//_}
         onlinexml=${cachedir}/${filename}.xml
-        wget -t 1 --no-http-keep-alive --no-cache --no-cookies --ignore-length --content-on-error -L -E -O - "$url" > "$onlinexml"
+        wget -nv -t 1 --no-http-keep-alive --no-cache --no-cookies --ignore-length --content-on-error -L -E -O - "$1" > "$onlinexml"
         if [[ $(stat -c %s "$onlinexml" 2>/dev/null) -ge 2 ]]
         then
-            printf '%s\n' "${url} ${onlinexml##*/}" >> "$table"
-            printf '%s\n' "SUCCESS $(date +%Y-%m-%d_%H:%M) URL:${url} synchronized." 1>&3
+            printf '%s\n' "${1} ${onlinexml##*/}" >> "$table"
+            printf '%s\n' "SUCCESS $(date +%Y-%m-%d_%H:%M) URL:${1} synchronized." 1>&3
         else
             rm -- "$onlinexml"
-            printf '%s\n' "FAIL $(date +%Y-%m-%d_%H:%M) URL:${url} unattainable." 1>&3
+            printf '%s\n' "FAIL $(date +%Y-%m-%d_%H:%M) URL:${1} unattainable." 1>&3
             return 1
         fi
     else
-        printf '%s\n' "FAIL $(date +%Y-%m-%d_%H:%M) URL:${url} does not match." 1>&3
+        printf '%s\n' "FAIL $(date +%Y-%m-%d_%H:%M) URL:${1} does not match." 1>&3
         return 1
     fi
 }
@@ -40,29 +45,24 @@ __feedcacher_do()
 export -f __feedcacher_do
 
 # Check connection status.
-if ! ping -c1 google.com 1>/dev/null 2>&1
-then
-    if ! wget -O - google.com 1>/dev/null 2>&1
-    then
-        # We are probably offline.
-        { echo "ERROR We are probably offline." 1>&2 ; exit 1 ; }
-    fi
-fi
+ping -c1 google.com 1>/dev/null 2>&1 ||
+wget -O - google.com 1>/dev/null 2>&1 ||
+{ echo "ERROR We are probably offline." 1>&2 ; exit 1 ; }
 
-[[ ! -d $cachedir ]] && mkdir -p "$cachedir"
+[[ -d $cachedir ]] || mkdir -p -- "$cachedir"
 [[ -f $reportnew ]] && rm -- "$reportnew"
 [[ -f $table ]] && rm -- "$table"
-[[ -d $cachedir ]] && { rm -r -- "$cachedir" ; mkdir -p "$cachedir" ; }
+[[ -d $cachedir ]] && { rm -r -- "$cachedir" ; mkdir -p -- "$cachedir" ; }
 
 tput bold setaf 7 ; printf '\n%s\n' "CHECK" ; tput sgr0
 exec 3>>"$reportnew"
-grep -v -e '^#http' "$urllist" | xargs -n 1 -P 8 -i bash -c '__feedcacher_do "$@"' _ {} +
+xargs -a "$urllist" -n 1 -P 8 -i bash -c '__feedcacher_do $@' _ {} +
 exec 3<&-
 
 if [[ $(stat -c %s "$reportnew" 2>/dev/null) -ge 2 ]]
 then
     tput bold setaf 7 ; printf '\n%s\n' "REPORT" ; tput sgr0
-    sed -n p "$reportnew"
+    cat "$reportnew"
 else
     printf '%s\n' "ERROR $(date +%Y-%m-%d_%H:%M) Report has not been created." 1>&2
 fi

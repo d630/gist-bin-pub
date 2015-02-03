f9703ffefef779fd68083ca65170ccb49ec99f69
D630 <D630@users.noreply.github.com>
2015-02-04 00:22:37 +0100

    backup-apt: init

---
+++ backup-apt/backup-apt.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Paketlisten
+
+sudo rm -r -- "${X_XDG_BACKUPS_DIR}/paketlisten" &&
+mkdir -- "${X_XDG_BACKUPS_DIR}/paketlisten" &&
+cd -- "${X_XDG_BACKUPS_DIR}/paketlisten" && {
+    COLUMNS=200 dpkg-query -l > "packages_list.list"
+    dpkg --get-selections | awk '!/deinstall|purge|hold/ {print $1}' > "packages.list"
+    apt-mark showauto > "package-states-auto"
+    apt-mark showmanual > "package-states-manual"
+    find /etc/apt/sources.list* -type f -name '*.list' -exec bash -c 'echo -e "\n## $1 " ; grep "^[[:space:]]*[^#[:space:]]" ${1}' _ {} \; > "sources.list.save"
+    sudo cp -- "/etc/apt/trusted.gpg" "trusted-keys.gpg"
+}

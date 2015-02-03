24d3882616807f4bd32c33ea0ca879d33ac81b81
D630 <D630@users.noreply.github.com>
2015-02-04 00:29:13 +0100

    backup-programs: update

--- backup-programs/backup-programs.sh
+++ backup-programs/backup-programs.sh
@@ -3,3 +3,4 @@
 # List all programs
 
 ls $(printf '%s ' ${PATH//:/ }) "/usr/bin/X11" | xargs whatis | sort | uniq > "${X_XDG_BACKUPS_DIR}/Programme.txt"
+find "/var/lib/dpkg/info/" -daystart \( -name \*.list -a -mtime -7 \) | sed 's/.list$//;s/.*\///' | sort > "${X_XDG_BACKUPS_DIR}/software_7_days.txt"

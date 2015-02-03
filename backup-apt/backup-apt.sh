e7701ea33fca60b09bfb929011b7a8fbe35e3708
D630 <D630@users.noreply.github.com>
2015-02-04 00:51:17 +0100

    backup-apt: update

--- backup-apt/backup-apt.sh
+++ backup-apt/backup-apt.sh
@@ -2,8 +2,8 @@
 
 # Paketlisten
 
-sudo rm -r -- "${X_XDG_BACKUPS_DIR}/paketlisten" &&
-mkdir -- "${X_XDG_BACKUPS_DIR}/paketlisten" &&
+sudo rm -r -- "${X_XDG_BACKUPS_DIR}/paketlisten"
+mkdir -p -- "${X_XDG_BACKUPS_DIR}/paketlisten"
 cd -- "${X_XDG_BACKUPS_DIR}/paketlisten" && {
     COLUMNS=200 dpkg-query -l > "packages_list.list"
     dpkg --get-selections | awk '!/deinstall|purge|hold/ {print $1}' > "packages.list"

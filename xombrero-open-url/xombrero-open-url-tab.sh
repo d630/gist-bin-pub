f08a04e7b6ec707a874686d2ab0ea4f537f03d84
D630 <D630@users.noreply.github.com>
2015-02-27 21:14:36 +0100

    xombrero-open-url: use tac

--- xombrero-open-url/xombrero-open-url-tab.sh
+++ xombrero-open-url/xombrero-open-url-tab.sh
@@ -5,7 +5,8 @@ count="$(pgrep -c xombrero)"
 file_uris="${X_XDG_LOG_HOME}/urls.log" \
 uri="$(
     grep -oP -e '((http|https|gopher|ftp|ftps|webdav|webdavs|dav|davs):(//)?[^ <>"\t]*|(www|ftp)[0-9]?\.[-a-z0-9.]+)[^ .,;\t\n\r<">\):]?[^, <>"\t]*[^ .,;\t\n\r<">\):]' "$file_uris" | \
-    #tac | fzf -e -i +s
+    tac | \
+    #fzf -e -i +s
     menu.sh "dmenu2" ">"
     )"
 

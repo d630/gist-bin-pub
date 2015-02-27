2404c3474ae2eab8a4d23c3a8c7cbfa69fcba27b
D630 <D630@users.noreply.github.com>
2015-02-27 20:36:08 +0100

    xwinreg-scripts: update

--- xwinreg-scripts/xwinreg-loop.sh
+++ xwinreg-scripts/xwinreg-loop.sh
@@ -2,21 +2,32 @@
 
 # Do layouting automatically (dynamically)
 
+declare \
+    win_active= \
+    win_active_old=
+
 while xev -root -event property | grep -q "(_NET_CLIENT_LIST).*state PropertyNewValue"
 do
-    if source "${XWINREG_TMP_FILE:-${TMPDIR:-/tmp}/xwinreg_default.tmp}"
+    read -r _ _ _ _ win_active < <(xprop -root -notype _NET_ACTIVE_WINDOW)
+    if [[ $(xprop -id "$win_active" WM_CLASS) =~ xtermj[12] || $(xprop -id "${win_active_old:-${win_active}}" WM_CLASS) =~ xtermj[12] ]]
     then
-        {
-            get-xids.sh | \
-            xwinpp print -I - --visible -D curr | \
-            xwinreg ${_xwinreg_options[command]}
-        } || {
-            source <(get-xids.sh | xwinpp print -I - --visible -D curr -P 0)
-            wmctrl -i -r "$_xwinpp_win_xid" -b "add,maximized_vert,maximized_horz"
-        }
+        :
     else
-        get-xids.sh | \
-        xwinpp print -I - --visible -D curr -P end | \
-        xwinreg -I - -L 1,1,maxi,alias:0,west -L 2,max,horiz,alias:0,east
+        if source "${XWINREG_TMP_FILE:-${TMPDIR:-/tmp}/xwinreg_default.tmp}"
+        then
+            {
+                get-xids.sh | \
+                xwinpp print -I - --visible -D curr | \
+                xwinreg ${_xwinreg_options[command]}
+            } || {
+                source <(get-xids.sh | xwinpp print -I - --visible -D curr -P 0)
+                wmctrl -i -r "$_xwinpp_win_xid" -b "add,maximized_vert,maximized_horz"
+            }
+        else
+            get-xids.sh | \
+            xwinpp print -I - --visible -D curr -P end | \
+            xwinreg -I - -L 1,1,maxi,alias:0,west -L 2,max,horiz,alias:0,east
+        fi
     fi
+    win_active_old=$win_active
 done 2>/dev/null
